1651190ef72b61e35641b5945fa4e4ccc106c935
Mahnoor_Ghaffar <MahnoorGhaffar@gmail.com>
2024-03-07 10:26:54 -0500

    typescript code

---
+++ Taskno-25/main.ts
@@ -0,0 +1,12 @@
+//Qno25
+//Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+//Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+let alienCOLOR:string="green";
+if (alienCOLOR==="green"){
+    console.log("you just earned 5 points.");
+}
+//Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+let alien_COLOR:string="green";
+if (alien_COLOR==="red"){
+    console.log("you just earned 5 points.");
+}
--- Taskno-29/main.ts
+++
@@ -1,2 +0,0 @@
-//Qno-29
-//Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
